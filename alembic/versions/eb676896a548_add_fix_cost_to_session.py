3edaa7c002b595d8ad32963c1b72aaec1aeefe6a
dory <minhduc.xd.93@gmail.com>
2026-05-18 14:47:37 +0100

    Fix fix_cost migration placement

--- alembic/versions/eb676896a548_add_fix_cost_to_session.py
+++ alembic/versions/eb676896a548_add_fix_cost_to_session.py
@@ -20,9 +20,12 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-    pass
+    op.add_column(
+        "cleaning_sessions",
+        sa.Column("fix_cost", sa.Float(), nullable=False, server_default="0"),
+    )
 
 
 def downgrade() -> None:
     """Downgrade schema."""
-    pass
+    op.drop_column("cleaning_sessions", "fix_cost")
