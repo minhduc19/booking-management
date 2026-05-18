79ae97e27dc33d659328dd7276787f463110e549
dory <minhduc.xd.93@gmail.com>
2026-05-18 14:32:14 +0100

    Add fix_cost to initial Alembic cleaning_sessions table

--- alembic/versions/ddfbfd3c8356_create_tables.py
+++ alembic/versions/ddfbfd3c8356_create_tables.py
@@ -101,6 +101,7 @@ def upgrade() -> None:
             sa.Column("hours", sa.Integer(), nullable=False),
             sa.Column("minutes", sa.Integer(), nullable=False),
             sa.Column("notes", sa.String(), nullable=True),
+            sa.Column("fix_cost", sa.Float(), nullable=False, server_default="0"),
             sa.ForeignKeyConstraint(["cleaner_id"], ["cleaners.id"]),
             sa.PrimaryKeyConstraint("id"),
         )
