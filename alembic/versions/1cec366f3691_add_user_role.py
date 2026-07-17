642b76e822640ce0dcc117cf8b1149a1ec39a5e4
Dory Nguyen <dory.nguyen@reading.ac.uk>
2026-07-17 16:31:39 +0100

    prepare alembic revision for user role

---
+++ alembic/versions/1cec366f3691_add_user_role.py
@@ -0,0 +1,28 @@
+"""add user role
+
+Revision ID: 1cec366f3691
+Revises: b1a2c3d4e5f6
+Create Date: 2026-07-17 16:31:06.595029
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '1cec366f3691'
+down_revision: Union[str, Sequence[str], None] = 'b1a2c3d4e5f6'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
